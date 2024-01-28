cf03a07b845e3e5c571e772e008b38470a058c34
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2024-01-29 02:11:16 +0600

    Navbar added + banner sign changed

--- src/app/page.js
+++ src/app/page.js
@@ -16,12 +16,12 @@ const Homepage = () => {
       <div className="diff aspect-[16/9]">
         <div className="diff-item-1">
           <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
-            DAISY
+            ABMART
           </div>
         </div>
         <div className="diff-item-2">
           <div className="bg-base-200 text-9xl font-black grid place-content-center">
-            DAISY
+            ABMART
           </div>
         </div>
         <div className="diff-resizer"></div>
